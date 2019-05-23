188cb163c20ec877c12908f03da29edc8fc04a1c
tympanix <tympanix@gmail.com>
2019-05-24 01:50:36 +0200

    fix: program file as argument

--- calc.go
+++ calc.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/tympanix/gocalc/parser"
 	"github.com/tympanix/gocalc/scanner"
 )
 
 func main() {
+	if len(os.Args) <= 1 {
+		log.Fatalln("Missing program arguments")
+	}
 
-	s, err := scanner.NewFromFile("program.p")
+	s, err := scanner.NewFromFile(os.Args[1])
 
 	if err != nil {
 		log.Fatalln(err)
