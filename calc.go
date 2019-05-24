dc99ca3e4e296dd95219c70239cd714c0fa03a0b
tympanix <tympanix@gmail.com>
2019-05-24 02:02:42 +0200

    feat: cli flags

--- calc.go
+++ calc.go
@@ -1,30 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/tympanix/gocalc/parser"
 	"github.com/tympanix/gocalc/scanner"
 )
 
+var (
+	verbose = flag.Bool("v", false, "verbose")
+)
+
 func main() {
-	if len(os.Args) <= 1 {
+
+	flag.Parse()
+
+	if flag.NArg() < 1 {
 		log.Fatalln("Missing program arguments")
 	}
 
-	s, err := scanner.NewFromFile(os.Args[1])
+	s, err := scanner.NewFromFile(flag.Arg(0))
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	p := parser.New(s)
-
 	n := p.ParseProgram()
 
 	fmt.Println(n.Calc())
 
-	n.Print()
+	if *verbose {
+		n.Print()
+	}
 }
